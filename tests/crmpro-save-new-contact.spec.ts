794473a2b398d8cfaeabb5037fac7864633dea0b
AroSebastine <arosebastinenishanth@gmail.com>
2025-10-08 20:35:11 +0530

    tried fixing crm pro new contact page by directly navigating to that page as mouse hover didnt work in old frame/frameset

--- tests/crmpro-save-new-contact.spec.ts
+++ tests/crmpro-save-new-contact.spec.ts
@@ -11,7 +11,9 @@ test('crmpro create a new contact', async({ page }) => {
     await page.getByRole('button', { name: "Login" }).click()
 
     // await page.frameLocator('frame[name="mainpanel"]').getByTitle('Contacts').waitFor( {state: "visible"} )
-    await page.frameLocator('frame[name="mainpanel"]').getByTitle('Contacts').hover({ force: true })
-    await page.frameLocator('frame[name="mainpanel"]').getByRole('link', { name: 'New Contact' }).click() 
-
+    // await page.frameLocator('frame[name="mainpanel"]').getByTitle('Contacts').hover({ force: true })
+    // await page.pause()
+    // await page.frameLocator('frame[name="mainpanel"]').getByRole('link', { name: 'New Contact' }).click() 
+    await page.goto('https://classic.freecrm.com/index.cfm?CFID=4316313&CFTOKEN=f0b8e2b76615d45-C1F63B93-D375-67DD-551850D3E39C3118&jsessionid=b6309de60fcc7415201562124d2985253717')
+    await page.pause()
 })
