69f36343761e0374fb5b2a24e28e586ace646873
AroSebastine <arosebastinenishanth@gmail.com>
2025-10-08 20:36:36 +0530

    added tests for js alerts, confirm, promt

---
+++ tests/jsalerts.spec.ts
@@ -0,0 +1,27 @@
+import { test, expect } from '@playwright/test'
+
+test('js alerts tests', async({ page }) => {
+    await page.goto('https://the-internet.herokuapp.com/javascript_alerts')
+
+    // alert - only OK button
+
+    // confirm - OK and Cancel buttons
+
+    // prompt - requests an user input  + OK and Cancel
+
+    page.on('dialog', async dialog => {
+        console.log(dialog.message());
+        console.log(dialog.type());
+        // await dialog.accept()
+        // await dialog.dismiss()
+        await dialog.accept('Aro is smart')
+    })
+
+    await page.getByRole('button', { name: "Click for JS Alert" }).click()
+    await page.getByRole('button', { name: "Click for JS Confirm" }).click()
+    await page.waitForTimeout(2000)
+    await page.getByRole('button', { name: "Click for JS Prompt" }).click()
+
+    await page.pause()
+
+})
