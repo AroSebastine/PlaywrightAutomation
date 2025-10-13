51a02c9db9f9d47c7a9e740ce9e72a9c4b8432fb
AroSebastine <arosebastinenishanth@gmail.com>
2025-10-13 08:05:09 +0530

    added iframe + shadow dom tests

--- tests/shadow-dom.spec.ts
+++ tests/shadow-dom.spec.ts
@@ -1,10 +1,19 @@
 import { test, expect} from '@playwright/test'
 
 test('shadow dom tests', async({ page }) => {
+    
     await page.goto('https://selectorshub.com/iframe-in-shadow-dom/')
 
     await page.locator("#kils").fill('Aro')
     await page.locator("#pizza").fill('classic veg reg pizza')
+    
+
+    // tea
+
+    await page.goto('https://selectorshub.com/shadow-dom-in-iframe/')
+
+    const foodTimeFrame = page.frameLocator('#pact')
+    await foodTimeFrame.locator('#tea').fill('Of course yes, Aro loves tea.')
     await page.pause()
 
 
