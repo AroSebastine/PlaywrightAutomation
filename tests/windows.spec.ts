3c6a6a68b16ff9ef4d87a08e454e9fdfceb6013e
AroSebastine <arosebastinenishanth@gmail.com>
2025-10-17 13:45:53 +0530

    added pages method to handle new windows

---
+++ tests/windows.spec.ts
@@ -0,0 +1,48 @@
+import { test, expect, Browser, BrowserContext, Page, chromium } from '@playwright/test'
+
+let browser: Browser
+let context: BrowserContext
+let page: Page
+
+test('new windows test', async() => {
+
+    browser = await chromium.launch({
+        headless: false,
+        channel: 'chrome'
+    })
+
+    context = await browser.newContext()
+    page = await context.newPage()
+
+    await page.goto('https://www.orangehrm.com/en/contact-sales')
+    const OrangeHRMTitle = await page.title()
+
+    const allowCookieButton = page.getByRole('button', { name: 'Allow all' })
+
+    page.on('framenavigated', async () => {       
+        
+        if(await allowCookieButton.isVisible()) {
+            await allowCookieButton.click()
+        }
+    })
+
+    await page.locator('//a[contains(@href,"linkedin")]').click()
+    await page.locator('//a[contains(@href,"facebook")]').click()
+    await page.locator('//a[contains(@href,"youtube")]').click()
+
+    await page.waitForTimeout(3000)
+
+    const pages: Page[] = context.pages()    
+    
+    for(let eachPage of pages) {
+        console.log(await eachPage.title());
+        if(await eachPage.title() !== OrangeHRMTitle) {
+            await eachPage.close()
+        }        
+    }
+
+    await pages[0].bringToFront()
+    await page.getByRole('textbox', { name: 'Full Name' }).fill('Smart Aro')
+
+    await page.waitForTimeout(3000)
+})
