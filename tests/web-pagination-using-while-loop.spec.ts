f0b5b55cb0ff8fd269bc3b2a1cb850621cd70cc4
AroSebastine <arosebastinenishanth@gmail.com>
2025-11-11 21:52:09 +0530

    updated web pagination test using while loop

--- tests/web-pagination-using-while-loop.spec.ts
+++ tests/web-pagination-using-while-loop.spec.ts
@@ -4,13 +4,15 @@ import { practicePages } from '../data/login.ts'
 test('web pagination while loop', async ({ page }) => {
     await page.goto(practicePages['selectors-hub-xpath'])
     const countryToSelect = 'India'
+    const secoundCountryToSelect = 'Russia'
 
     while(true) {
 
-        const country = page.locator(`//td[text()="${countryToSelect}"]`)
+        const country = page.locator(`//td[text()="${countryToSelect}" or text()="${secoundCountryToSelect}"]`)
         const listOfCountryToSelect = await country.all()
         const isCountryAvailableInPage = listOfCountryToSelect.length > 0        
-        const nextButton = page.getByRole('link', { name: 'Next' })       
+        const nextButton = page.getByRole('link', { name: 'Next' })   
+        // const nextButton = page.locator('//button[contains(@class, "dt-paging-button") and contains(@class, "next")]')    
 
         if(isCountryAvailableInPage) {
             for(const eachCountry of listOfCountryToSelect) {
