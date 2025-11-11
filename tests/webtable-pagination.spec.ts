233dba3f015847f3e49f9f59082c4289c6d32d69
AroSebastine <arosebastinenishanth@gmail.com>
2025-11-11 05:46:21 +0530

    more updated to web pagination test

--- tests/webtable-pagination.spec.ts
+++ tests/webtable-pagination.spec.ts
@@ -2,32 +2,44 @@ import { practicePages } from '../data/login.ts'
 import { test, expect, Locator } from '@playwright/test'
 
 test('xpath practice, web pagination tests', async ({ page }) => {
+
+    const singleSelection = false
+    const country = "India"
+    let i = 0
     await page.goto(practicePages['selectors-hub-xpath'])
 
     await mainLogic()
 
     async function mainLogic() {
-        
+
         const allRows: Locator[] = await page.locator('//table[@id="tablepress-1"]//tbody/tr').all()
-        const allCountriesInCurrentPage: string[] = await page.locator('//table[@id="tablepress-1"]//tbody/tr/td[@class="column-5"]').allInnerTexts() 
+        const allCountriesInCurrentPage: string[] = await page.locator('//table[@id="tablepress-1"]//tbody/tr/td[@class="column-5"]').allInnerTexts()
         const nextPage: Locator = page.locator('//button[contains(@class, "dt-paging-button") and contains(@class, "next")]')
 
-        if (allCountriesInCurrentPage.includes('Hong Kong')) {            
+        if (allCountriesInCurrentPage.includes(country)) {
 
-            for (const eachRow of allRows) {                
-                const country: string = await eachRow.locator('//td[@class="column-5"]').innerText()                
-                
-                if (country === "Hong Kong") {    
+            for (const eachRow of allRows) {
+                i++
+                const countryInRow: string = await eachRow.locator('//td[@class="column-5"]').innerText()
 
+                if (countryInRow === country) {
                     await eachRow.locator('//td[@class="column-1"]/input').check()
-                    break
+                    if (singleSelection) {
+                        break
+                    }
                 }
             }
-            
+
+            if (await nextPage.isEnabled({ timeout: 3000 }) && !singleSelection) {
+                await nextPage.click()
+                await mainLogic()
+            }
+
         } else if (await nextPage.isEnabled({ timeout: 3000 })) {
             await nextPage.click()
             await mainLogic()
-        } else {
+        }
+        else {
             console.log('The country Honk Kong is not found');
         }
     }
